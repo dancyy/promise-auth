0344a70a5ab4bbe4bb181d04d4f6441a67f089b6
dancyy <kendell.dancy@codeimmersives.com>
2018-08-01 11:11:46 -0400

    add products to product page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var ProductController = require('../controllers/ProductController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+ ProductController.getAllProducts({})
+  .then((products) => {
+    res.render('index', { products : products });
+  }) 
+  .catch((err) => {
+    res.render('error', {message: 'Error', error: error});
+  });
 });
 
 module.exports = router;
