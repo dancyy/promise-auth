6896d4eae8fd2767cc034ba5acfb381f1422318d
dancyy <kendell.dancy@codeimmersives.com>
2018-08-01 12:12:43 -0400

    set up create product form

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var ProductController = require('../controllers/ProductController');
+var multer = require('multer');
+var uuid = require('uuid');
+
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './public/images')
+    },
+    filename: function(req, file, cb) {
+        cb(null, uuid() + '.jpg');
+    }
+});
 
 /* GET the Product page. */
 router.get('/', function(req, res, next) {
@@ -25,8 +36,17 @@ router.get('/createproduct', function(req, res, next) {
     res.render('createproduct');
 });
 
+var upload = multer({storage: storage});
 
-router.post('/createproduct', function(req, res, next) {
+router.post('/createproduct', upload.single('productPicture'), function(req, res, next) {
+    let productPicture;
+    if(req.file) {
+        productPicture = req.file.filename;
+    } else {
+        productPicture = 'noimage.jpg';
+    }
+    req.body.productPicture = productPicture;
+
     ProductController.createProduct(req.body)
         .then(product => {
             res.json({
